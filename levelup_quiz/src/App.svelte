c766f1a2c4f7c19c6d8bd0a593d0b6b2774d8450
Avik013 <gukasyan.spa@gmail.com>
2023-01-15 23:35:34 +0400

    feat(quiz): add quiz component with async random questions

--- levelup_quiz/src/App.svelte
+++ levelup_quiz/src/App.svelte
@@ -1,9 +1,13 @@
 <script>
-	
+    import Quiz from "./Quiz.svelte";
 </script>
 
-<h1>Hello A13!</h1>
+<h1>Level Up Quiz</h1>
+
+<Quiz />
 
 <style>
-
+    :global(h4) {
+        color: red;
+    }
 </style>
---
+++ levelup_quiz/src/Quiz.svelte
@@ -0,0 +1,27 @@
+<script>
+    let quiz = getQuiz();
+
+    async function getQuiz() {
+        const response = await fetch("https://opentdb.com/api.php?amount=10&category=21&type=multiple");
+
+        return response.json();
+    }
+
+    const updateQuestion = () => {
+        quiz = getQuiz();
+    }
+</script>
+
+<!-- HTML -->
+<button on:click={updateQuestion}>Get New Question</button>
+
+{#await quiz}
+    Loading...
+{:then data}
+    <p>{data.results[0]["question"]}</p>
+{/await}
+
+<!-- STYLE -->
+<style>
+
+</style>
